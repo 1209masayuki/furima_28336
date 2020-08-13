1b9ec0f57550b4dc6b5aff284c0cb015485d84e6
1209masayuki <xkiddo22@yahoo.co.jp>
2020-08-13 21:40:50 +0900

    rails g controller items index new createをしたらルーティングに勝手に記述された。
    よくわからないので一旦コメントアウト

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'items/index'
-  get 'items/new'
-  get 'items/create'
+  # get 'items/index'
+  # get 'items/new'
+  # get 'items/create'
+  root to: 'items#index'
   devise_for :users
   resources :items, only: [:index, :new, :create]
 end
