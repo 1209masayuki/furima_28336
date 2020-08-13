636e88fb7292a021b9ec95b5ed57f32fa3af97e1
1209masayuki <xkiddo22@yahoo.co.jp>
2020-08-13 19:19:02 +0900

    Itemモデルにアソシエーションとバリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+  has_one :purchase
+  belongs_to :user
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category, :sales_status, :shipping_fee_status, :prefecture, :scheduled_delivery
+
+  validates :prefecture, numericality: { other_than: 0, message: "can't be blank" }
+  validates :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id numericality: { other_than: 1 }
+  validates :name, :info, :category, :sales_status, :shipping_fee_status, :prefecture, :scheduled_delivery, :price, :user, :image presence: true
+  validates :price, format: {with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters."}
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
 end
