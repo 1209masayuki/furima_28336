20211fae8e358d03861d1f459a1e964481e534b9
1209masayuki <xkiddo22@yahoo.co.jp>
2020-08-25 11:59:55 +0900

    ユーザー管理機能　モデルの記述　アソシエーションとバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :items
+  has_many :purchases
+
+  validates :nickname, :email, :birth_date, presence: true
+  with_options presence: true do
+    validates :password, format: { with: /\A[a-zA-Z0-9]+\z/, message: 'Password Include both letters numbers' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'First name Full-width characters' }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Last name Full-width characters' }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'First name kana Full-width katakana characters' }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Last name kana Full-width katakana characters' }
+  end
 end
