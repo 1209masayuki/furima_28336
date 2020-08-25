c5c8d4a67537f4fe7683d3296237ae176e92ab9b
1209masayuki <xkiddo22@yahoo.co.jp>
2020-08-25 11:57:23 +0900

    ユーザー管理機能　特に何もしていない

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+  # def create
+  #   if @user = User.new(prams[:id])
+  #     @user.save
+  #     return redirect_to items_path
+  #   else
+  #     render "new"
+  #   end
+end
