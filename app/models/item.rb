44515b8881cec9dbf5b4c1e01086bd5f0eab8d7a
1209masayuki <xkiddo22@yahoo.co.jp>
2020-08-25 12:14:23 +0900

    商品[一覧表示、詳細表示、編集、削除]機能　itemモデルの　アソシエーションとバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,17 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :sales_status
+  belongs_to_active_hash :shipping_fee_status
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :scheduled_delivery
+
   has_one :purchase
   belongs_to :user
   has_one_attached :image
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category, :sales_status, :shipping_fee_status, :prefecture, :scheduled_delivery
 
-  validates :prefecture, numericality: { other_than: 0, message: "can't be blank" }
-  validates :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, numericality: { other_than: 1 }
-  validates :name, :info, :category, :sales_status, :shipping_fee_status, :prefecture, :scheduled_delivery, :price, :user, :image, presence: true
-  validates :price, format: {with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters."}
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
+  validates :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price, :user_id, :image, presence: true
+  validates :price, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters.' }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
 end
