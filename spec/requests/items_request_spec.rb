9e0fcc4f22a901d80f6e1a44f21bb1575fd6463a
1209masayuki <xkiddo22@yahoo.co.jp>
2020-08-28 23:39:16 +0900

    itemテストコード

--- spec/requests/items_request_spec.rb
+++ spec/requests/items_request_spec.rb
@@ -1,26 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe "Items", type: :request do
-
-  describe "GET /index" do
-    it "returns http success" do
-      get "/items/index"
+RSpec.describe 'Items', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/items/index'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /new" do
-    it "returns http success" do
-      get "/items/new"
+  describe 'GET /new' do
+    it 'returns http success' do
+      get '/items/new'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/items/create"
+  describe 'GET /create' do
+    it 'returns http success' do
+      get '/items/create'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
