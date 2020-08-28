4bd4323a52a48d77f2f500565061083b5fae4222
1209masayuki <xkiddo22@yahoo.co.jp>
2020-08-28 23:44:38 +0900

    ユーザーモデルの編集　引数からバリデーションに移動

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,9 @@ class User < ApplicationRecord
   has_many :items
   has_many :purchases
 
-  validates :nickname, :email, :birth_date, presence: true
+  validates :email, :birth_date, presence: true
   with_options presence: true do
+    validates :nickname, length: { maximum: 40 }
     validates :password, format: { with: /\A[a-zA-Z0-9]+\z/, message: 'Password Include both letters numbers' }
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'First name Full-width characters' }
     validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Last name Full-width characters' }
