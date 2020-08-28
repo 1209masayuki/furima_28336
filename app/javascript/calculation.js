69faedbc13d15ab5f67d5203fc8142506a1733bc
1209masayuki <xkiddo22@yahoo.co.jp>
2020-08-28 23:42:52 +0900

    体裁を整えた

--- app/javascript/calculation.js
+++ app/javascript/calculation.js
@@ -1,23 +1,10 @@
 function calculation(){
-// var price = document.getElementById("item-price");
-// price.setAttribute("value", "");
-const keydown = document.getElementById("item-price");
-
-keydown.addEventListener('keyup', function(){
-const input_message = document.getElementById("item-price").value;
-
-console.log(input_message);
-// console.log(keydown);
-if (input_message >= 300 && input_message <= 9999999){
-document.getElementById("add-tax-price").innerHTML = (input_message * 0.1);
-document.getElementById("profit").innerHTML = input_message - (input_message * 0.1);
-}});
+  const keyup = document.getElementById("item-price");
+  keyup.addEventListener('keyup', function(){
+    const input_message = document.getElementById("item-price").value;
+    if (input_message >= 300 && input_message <= 9999999){
+      document.getElementById("add-tax-price").innerHTML = parseInt((input_message * 0.1), 10);
+      document.getElementById("profit").innerHTML = parseInt(input_message - (input_message * 0.1), 10);
+  }});
 };
-window.addEventListener("load", calculation)
-
-// input_message = `#{price}`;
-// /console.log(price)
-// const add_tax = document.getElementById("add-tax-price");
-// console.log(add_tax)
-// const profit = document.getElementById("profit");
-// console.log(profit)
+window.addEventListener("load", calculation);
